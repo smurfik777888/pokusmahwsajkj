f99a414700c1a92530eecd1436d7d3f964efa47f
Dtp223 <bronte@iwristlu.com>
2024-10-02 15:55:32 +0300

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://bronsed.co.ua'
+	const proxyUrl = 'https://pokkippkatropa.cloud'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
