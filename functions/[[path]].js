6f2723fdd6f114a9e38eeb368d3d7e8743db3afb
Dtp223 <bronte@iwristlu.com>
2024-10-04 09:37:36 +0300

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://aboba.biz.ua'
+	const proxyUrl = 'https://startblacksrats.info'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
